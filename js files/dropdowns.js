b1770c4390f561624058ae8ee3e9ad1e9f00d1a6
JamesFel <jafkm7@mst.edu>
2018-08-25 17:59:31 -0500

    Simplified some code and added persistence in user selections where possible.

--- js files/dropdowns.js
+++ js files/dropdowns.js
@@ -21,7 +21,7 @@ removeOptions = function(dropdown)
 
 populateList = function(db, mission_num, key, offset, ea, listToFill, exclude=null, eOffset=0)
 {
-    for (secondaryKey in db[key])
+    for (let secondaryKey in db[key])
     {
         let item = db[key][secondaryKey]
         let rob = item[offset + ea]
@@ -40,6 +40,50 @@ populateList = function(db, mission_num, key, offset, ea, listToFill, exclude=nu
     }
 }
 
+checkCurrentSelection = function()
+{
+    "use strict"
+    let selectorIds = [
+        "left-shoulder-selector",
+        "right-shoulder-selector",
+        "body-selector",
+        "left-arm-selector",
+        "right-arm-selector",
+        "wanzer-legs-selector",
+        "backpack-selector",
+        "right-hand-selector",
+        "left-hand-selector"
+    ]
+    let response = {}
+
+    for(let i=0;i < selectorIds.length; i++)
+    {
+        let current = document.getElementById(selectorIds[i]);
+
+        if (current != undefined)
+        {
+            current = current.value;
+            if(current[0] != '-') {response[selectorIds[i]] = current;}
+        }
+    }
+
+    return response;
+}
+
+updateSelection = function(selectorIdList, replacementList, previousSelections)
+{
+    "use strict"
+    for(let i=0; i < selectorIdList.length; i++)
+    {
+        let currentId = selectorIdList[i];
+        replaceOptions(replacementList, currentId)
+        if(currentId in previousSelections && replacementList.includes(previousSelections[currentId]))
+        {
+            document.getElementById(currentId).value=previousSelections[currentId];
+        }
+    }
+}
+
 updateDropdownLists = function ()
 {
     "use strict"
@@ -53,8 +97,9 @@ updateDropdownLists = function ()
         "body-selector",
         "left-arm-selector",
         "right-arm-selector",
-        "wanzer-legs-selector",
+        "wanzer-legs-selector"
     ]
+    let currentlySelected = checkCurrentSelection();
 
     //resetting the arrays
     wanzerList.length = 0;
@@ -90,33 +135,30 @@ updateDropdownLists = function ()
     populateList(db, mission_num, "backpacks", 5, ea, backpackListP,[4, 6, 8], 2)
     populateList(db, mission_num, "backpacks", 5, ea, backpackListC, [30, 60, 90], 3)
 
-    for(let i=0; i < wanzerPartsSelectorIdList.length; i++)
-    {
-        replaceOptions(wanzerList, wanzerPartsSelectorIdList[i])
-    }
-
-    for(let i=0; i < handWeaponsSelectorIdList.length; i++)
-    {
-        replaceOptions(handWeaponList, handWeaponsSelectorIdList[i])
-    }
-
-    for(let i=0; i < shoulderWeaponSelectorIdList.length; i++)
-    {
-        replaceOptions(shoulderWeaponList, shoulderWeaponSelectorIdList[i])
-    }
-
-    for(let i=0; i < shieldSelectorIdList.length; i++)
-    {
-        replaceOptions(shieldList, shieldSelectorIdList[i])
-    }
+    updateSelection(wanzerPartsSelectorIdList, wanzerList, currentlySelected);
+    updateSelection(handWeaponsSelectorIdList, handWeaponList, currentlySelected);
+    updateSelection(shoulderWeaponSelectorIdList, shoulderWeaponList, currentlySelected);
+    updateSelection(shieldSelectorIdList, shieldList, currentlySelected);
 
     if(document.getElementById("capacity-power").value == "p")
     {
-        replaceOptions(backpackListP, "backpack-selector")
+        updateSelection(["backpack-selector"], backpackListP, currentlySelected);
     }
     else
     {
-        replaceOptions(backpackListC, "backpack-selector")
+        updateSelection(["backpack-selector"], backpackListC, currentlySelected);
     }
 
+}
+
+updateHandList = function(hand)
+{
+    if(document.getElementById(hand + "-shield").value == "s")
+    {
+        replaceOptions(shieldList, hand + "-hand-selector")
+    }
+    else
+    {
+        replaceOptions(handWeaponList, hand + "-hand-selector")
+    }
 }
